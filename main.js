858336604ad1b5687f133dd13eb7c4fe5eb0a8bd
Caio Bernardo de Lima <cbernardodelima@ucf.edu>
2022-02-03 18:35:15 -0500

    did a homework

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstName = "Caio "; 
+var lastName = "Bernardo-de-Lima";
+var age = 24; 
+
+alert("Hello");
+alert("My name " + firstName + lastName);
+alert("I am " + age);
+
+console.log("Hello");
+console.log("My name is Caio Breder");
+console.log("I am 24 years old");
